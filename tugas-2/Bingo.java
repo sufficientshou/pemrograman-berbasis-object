9162d0cb96d5936e50a1c18467d18b2822df6a03
sufficientshou <192699143+sufficientshou@users.noreply.github.com>
2026-03-02 21:25:19 +0700

    feat: menambahkan tugas PBO(Matematika, Interface, Bingo)

---
+++ tugas-2/Bingo.java
@@ -0,0 +1,53 @@
+class TeksBingo {
+
+    TeksBingo() {
+
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println("B-I-N-G-O");
+        System.out.println("B-I-N-G-O");
+        System.out.println("B-I-N-G-O");
+        System.out.println("And Bingo was his name-o.\n");
+
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println("(clap)-I-N-G-O");
+        System.out.println("(clap)-I-N-G-O");
+        System.out.println("(clap)-I-N-G-O");
+        System.out.println("And Bingo was his name-o.\n");
+
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println("(clap)-(clap)-N-G-O");
+        System.out.println("(clap)-(clap)-N-G-O");
+        System.out.println("(clap)-(clap)-N-G-O");
+        System.out.println("And Bingo was his name-o.\n");
+
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println("(clap)-(clap)-(clap)-G-O");
+        System.out.println("(clap)-(clap)-(clap)-G-O");
+        System.out.println("(clap)-(clap)-(clap)-G-O");
+        System.out.println("And Bingo was his name-o.\n");
+
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println("(clap)-(clap)-(clap)-(clap)-O");
+        System.out.println("(clap)-(clap)-(clap)-(clap)-O");
+        System.out.println("(clap)-(clap)-(clap)-(clap)-O");
+        System.out.println("And Bingo was his name-o.\n");
+
+        System.out.println("There was a farmer who had a dog,");
+        System.out.println("And Bingo was his name-o.");
+        System.out.println("(clap)-(clap)-(clap)-(clap)-(clap)");
+        System.out.println("(clap)-(clap)-(clap)-(clap)-(clap)");
+        System.out.println("(clap)-(clap)-(clap)-(clap)-(clap)");
+        System.out.println("And Bingo was his name-o.\n");
+    }
+}
+
+public class Bingo {
+    public static void main(String[] args) {
+        new TeksBingo();
+    }
+}
